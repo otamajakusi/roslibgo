591230712f87990d610341f4b17f73a185b13bc2
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2021-03-13 21:53:23 +0900

    update topic_test

--- topic_test.go
+++ topic_test.go
@@ -5,7 +5,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"os/exec"
 	//"sync"
+	//"fmt"
 	"testing"
+	"time"
 )
 
 type StdMsgs_String struct {
@@ -28,27 +30,68 @@ func rosTopicEchoCmd(topic_name string) (chan int, chan string) {
 	return started, output
 }
 
+func rosTopicPubCmd(topic_name string, topic_type string, topic_msg string) {
+	cmd := exec.Command("rostopic", "pub", "-1", topic_name, topic_type, topic_msg)
+	cmd.Run()
+}
+
 // 1. (external process) launch rostopic echo
 // 2. publish message
 // 3. (external process) receive rostopic echo output
 // 4. compare 2 and 3
 func TestPublishWithRosTopicEcho(t *testing.T) {
-
 	ros, _ := NewRos("ws://localhost:9090")
 	ros.Run()
 
 	started, output := rosTopicEchoCmd("/foo/bar")
 
 	pub := NewTopic(ros, "/foo/bar", "std_msgs/String")
-	s, _ := json.Marshal(StdMsgs_String{Data: "Hello world!"})
-	<-started // wait echo process started
+	s, _ := json.Marshal(StdMsgs_String{Data: "Hello roslibgo"})
+	<-started // wait go routine started
 
 	pub.Publish(json.RawMessage(s))
-	assert.Equal(t, <-output, "data: \"Hello world!\"\n---\n")
+	assert.Equal(t, <-output, "data: \"Hello roslibgo\"\n---\n")
 }
 
-func TestSubscribe(t *testing.T) {
+func TestPublishWithSubscribe(t *testing.T) {
+	ros, _ := NewRos("ws://localhost:9090")
+	ros.Run()
+
+	// subscribe
+	sub := NewTopic(ros, "/foo/bar", "std_msgs/String")
+	output := make(chan string)
+	callback := func(data json.RawMessage) {
+		var msg StdMsgs_String
+		json.Unmarshal(data, &msg)
+		output <- msg.Data
+	}
+	sub.Subscribe(callback)
+	time.Sleep(1 * time.Second)
+
+	// publish
+	pub := NewTopic(ros, "/foo/bar", "std_msgs/String")
+	s, _ := json.Marshal(StdMsgs_String{Data: "Hello roslibgo"})
+	pub.Publish(json.RawMessage(s))
+
+	assert.Equal(t, <-output, "Hello roslibgo")
 }
 
-func TestUnsubscribe(t *testing.T) {
+func TestSubscribeWithRosTopicPub(t *testing.T) {
+	ros, _ := NewRos("ws://localhost:9090")
+	ros.Run()
+
+	// subscribe
+	sub := NewTopic(ros, "/foo/bar", "std_msgs/String")
+	output := make(chan string)
+	callback := func(data json.RawMessage) {
+		var msg StdMsgs_String
+		json.Unmarshal(data, &msg)
+		output <- msg.Data
+	}
+	sub.Subscribe(callback)
+	time.Sleep(1 * time.Second)
+
+	rosTopicPubCmd("/foo/bar", "std_msgs/String", "data: 'Hello roslibgo'")
+
+	assert.Equal(t, <-output, "Hello roslibgo")
 }
